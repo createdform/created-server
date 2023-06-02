06db5ac1153a8fbfd5cf08b8c41bb567509c6c80
devinreeks <devin@gotillit.com>
2023-06-02 20:16:41 +1000

    updated path

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	})
 
 	http.HandleFunc("/verify", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadFile("commit_hash.txt")
+		body, err := ioutil.ReadFile("/home/commit_hash.txt")
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("unable to read file: " + err.Error()))
